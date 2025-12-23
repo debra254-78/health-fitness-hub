d1494102f988865c92cbbdc4a94c0153a839b105
debra254-78 <debragatwirimwe@gmail.com>
2025-12-23 22:57:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleInfo()[
+    const info=
+    document.getElementById("info")
+    if(info.style.display==="none"){
+        info.style.display="block}"}else{
+            info.style.display="none"
+        }
+]
